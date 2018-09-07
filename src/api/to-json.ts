701b1e4e040a8a0870ff2fe3a3bc7d59a1356de0
Amit Shlomovits <amut00@gmail.com>
2018-09-07 12:15:31 +0300

    Added alias to tojson

--- src/api/to-json.ts
+++ src/api/to-json.ts
@@ -10,7 +10,9 @@ import {client} from '../index';
 export class ToJsonAPI {
     static init(app:express.Application) {
         app.get('/toJSON', ToJsonAPI.getTreeJson);
+        app.get('/getJSON', ToJsonAPI.getTreeJson);
         app.post('/toJSON', ToJsonAPI.getTreeJsonFromPath);
+        app.post('/getJSON', ToJsonAPI.getTreeJsonFromPath);
     }
 
     private static async getTreeJson(req:express.Request, res:express.Response):Promise<any> {
